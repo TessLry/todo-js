630ad2bb337bd70b14c9cf6687ef7bb795c26924
TessLry <lerayt@live.fr>
2023-07-13 00:35:24 +0200

    add some filters

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption= document.querySelector('.filter-todo');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 //Functions
 function addTodo (event) { 
@@ -59,5 +61,33 @@ function deleteCheck(e) {
     if(item.classList[0] === 'complete-btn') {
         const todo = item.parentElement;
         todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo (e) {
+    const todos = todoList.childNodes;
+    for(let i = 1; i < todos.length; i++) {
+        const todo = todos[i];
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                }
+                else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                }
+                else {
+                    todo.style.display = 'none';
+                }
+                break; 
+        }
     }
 }
