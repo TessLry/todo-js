ea7c589411e873a8c06b2a8b7266fddf976658e2
TessLry <lerayt@live.fr>
2023-07-12 23:41:21 +0200

    delete item into the list

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 function addTodo (event) { 
@@ -41,4 +42,22 @@ function addTodo (event) {
 
     //Clear Todo Input Value
     todoInput.value = "";
+}
+
+function deleteCheck(e) {
+   const item = e.target;
+
+   if(item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        //Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        });
+   }
+
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
